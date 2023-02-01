7e76764aad6a081e215e767772a4f1fa9644c214
SubhaPranav18 <subhainfotech90@gmail.com>
2023-02-01 13:00:19 +0530

    new codes A

--- src/test/java/org/iolo/LoginPages.java
+++ src/test/java/org/iolo/LoginPages.java
@@ -1,5 +1,18 @@
 package org.iolo;
 
-public class LoginPages {
 
-}
+
+public class LoginPages {
+	public static void main(String[] args) {
+		String String = "subha";
+		System.out.println(String);
+		boolean empty = String.isEmpty();
+		System.out.println(empty);
+	   
+				
+	}
+
+	}
+	
+
+
